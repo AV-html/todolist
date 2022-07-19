bc1d12c7977e1c45c06475e04b45eefd018e0b3a
Alexander Volkov <AV-html@yandex.ru>
2022-07-19 15:31:17 +0300

    Updates

---
+++ precache-manifest.d4aa6f7451ce616f841cdaeb3393d61e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96095cce3aa96ad7fe9d183da205a3ca",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "c590b7cd050aa1f69da1",
+    "url": "/todolist/static/css/main.16e9cc8a.chunk.css"
+  },
+  {
+    "revision": "e0d8841c428b4b45e874",
+    "url": "/todolist/static/js/2.f46e6ddd.chunk.js"
+  },
+  {
+    "revision": "c18546fe0cf2182f83e5d04d0130ce43",
+    "url": "/todolist/static/js/2.f46e6ddd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c590b7cd050aa1f69da1",
+    "url": "/todolist/static/js/main.5e7d0a63.chunk.js"
+  },
+  {
+    "revision": "471e54e937a3aef18e16",
+    "url": "/todolist/static/js/runtime-main.283d8678.js"
+  }
+]);
